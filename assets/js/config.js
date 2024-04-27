ab73effea0e7ff8b1682f8d4c882a4d3c1c5b41d
Elvin <elvinsrkrov@gmail.com>
2024-04-27 21:49:26 +0400

    responsive design updated

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
 export const API_KEY = "a3a17dd3-d5ba-4a69-b796-e2ac3fa11dcb";
-export const SPOONACULAR_API_URL = `https://api.spoonacular.com/recipes/parseIngredients`;
-export const SPOONACULAR_API_KEY = "1fed54ff99014540acce11d0955733fb";
+// export const SPOONACULAR_API_URL = `https://api.spoonacular.com/recipes/parseIngredients`;
+// export const SPOONACULAR_API_KEY = "622843b5344a4effbf4516c17ed810f3";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 13;
 export const MODAL_CLOSE_SEC = 2;
