15b583609f13f8d3b093a58661d69f376da14263
Elvin <elvinsrkrov@gmail.com>
2024-06-06 16:35:01 +0400

    responsive design has updated

--- src/js/config.js
+++ src/js/config.js
@@ -3,5 +3,5 @@ export const API_KEY = "a3a17dd3-d5ba-4a69-b796-e2ac3fa11dcb";
 export const SPOONACULAR_API_URL = `https://api.spoonacular.com/recipes/parseIngredients`;
 export const SPOONACULAR_API_KEY = "622843b5344a4effbf4516c17ed810f3";
 export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 13;
+export const RES_PER_PAGE = 15;
 export const MODAL_CLOSE_SEC = 2;
